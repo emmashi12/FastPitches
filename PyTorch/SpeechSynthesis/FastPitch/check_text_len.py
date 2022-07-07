bf28454822bc0e49b26e19b7402379d65402b65e
emmashi12 <38935478+emmashi12@users.noreply.github.com>
2022-07-07 15:10:49 +0100

    check len3

--- PyTorch/SpeechSynthesis/FastPitch/check_text_len.py
+++ PyTorch/SpeechSynthesis/FastPitch/check_text_len.py
@@ -4,7 +4,7 @@ from common.text.text_processing import TextProcessing
 from common.utils import load_filepaths_and_text
 
 dataset_path = '/exports/eddie/scratch/s2226669/LJSpeech-1.1/'
-data_path = ['/exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/s2226669_Wenjing_Shi/selfversion/PyTorch/SpeechSynthesis/FastPitch/filelists/ljs_audio_pitch_prom_text_train_v3.txt']
+data_path = ['/exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/s2226669_Wenjing_Shi/selfversion/PyTorch/SpeechSynthesis/FastPitch/filelists/ljs_audio_pitch_prom_text_train_v3_test.txt']
 audiopaths_and_text = load_filepaths_and_text(data_path, has_speakers=False)
 tp = TextProcessing('english_basic', ["english_cleaners_v2"], p_arpabet=1.0, get_count=True)
 log_file = open("incorrect_label.txt", "a")
