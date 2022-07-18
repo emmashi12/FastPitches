248cb6ae3017e5fe6116f8ebd0fa0d9d15a191a0
emmashi12 <38935478+emmashi12@users.noreply.github.com>
2022-07-18 12:32:20 +0100

    cwt

--- PyTorch/SpeechSynthesis/FastPitch/check_text_len.py
+++ PyTorch/SpeechSynthesis/FastPitch/check_text_len.py
@@ -3,7 +3,7 @@ import torch
 from common.text.text_processing import TextProcessing
 from common.utils import load_filepaths_and_text
 
-dataset_path = '/exports/eddie/scratch/s2226669/LJSpeech-1.1/'
+dataset_path = '/exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/s2226669_Wenjing_Shi/LJSpeech-1.1/'
 data_path = ['/exports/chss/eddie/ppls/groups/lel_hcrc_cstr_students/s2226669_Wenjing_Shi/selfversion/PyTorch/SpeechSynthesis/FastPitch/filelists/ljs_audio_pitch_prom_text_val.txt']
 audiopaths_and_text = load_filepaths_and_text(data_path, has_speakers=False)
 tp = TextProcessing('english_basic', ["english_cleaners_v2"], p_arpabet=1.0, get_count=True)
