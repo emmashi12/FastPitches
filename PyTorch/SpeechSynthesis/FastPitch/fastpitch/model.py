5a743d7744204b9a41cf14b054668b2c42a8174c
emmashi12 <38935478+emmashi12@users.noreply.github.com>
2022-07-27 23:30:58 +0100

    cwt

--- PyTorch/SpeechSynthesis/FastPitch/fastpitch/model.py
+++ PyTorch/SpeechSynthesis/FastPitch/fastpitch/model.py
@@ -423,6 +423,7 @@ class FastPitch(nn.Module):
                 b_pred_label = torch.argmax(b_pred_label, dim=1)
                 if use_gt_cwt_b and cwt_b_tgt is not None:
                     cwt_b_emb = self.cwt_b_emb(cwt_b_tgt)
+                    print(f'cwt_b_emb type: {cwt_b_emb.type()}')
                 else:
                     cwt_b_emb = self.b_emb(b_pred_label)
             else:
@@ -435,6 +436,7 @@ class FastPitch(nn.Module):
                     if use_gt_cwt_prom and cwt_prom_tgt is not None:
                         cwt_prom_tgt = cwt_prom_tgt.unsqueeze(1)  # [batch_size, 1, text_len]
                         cwt_prom_emb = self.cwt_prom_emb(cwt_prom_tgt)
+                        print(f'cwt_prom_emb type: {cwt_prom_emb.type()}')
                     else:
                         cwt_prom_emb = self.cwt_prom_emb(cwt_prom_pred)
                         cwt_prom_emb = cwt_prom_emb.transpose(1, 2)
