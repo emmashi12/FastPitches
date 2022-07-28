48c3b8af269eec0b31dbf7042d2c4e3efadff244
emmashi12 <38935478+emmashi12@users.noreply.github.com>
2022-07-28 23:00:25 +0100

    prom control

--- PyTorch/SpeechSynthesis/FastPitch/inference.py
+++ PyTorch/SpeechSynthesis/FastPitch/inference.py
@@ -253,6 +253,7 @@ def prepare_input_sequence(fields, device, symbol_set, text_cleaners,
             upsampled = upsampling_label(fields['prom_tensor'][i], fields['text_info'][i])[0]
             print(f'upsampled:\n {upsampled}')
             fields['prom_upsampled'].append(upsampled)
+        fields['prom'] = [fields['prom'][i] for i in order]
 
     if 'output' in fields:
         fields['output'] = [fields['output'][i] for i in order]
