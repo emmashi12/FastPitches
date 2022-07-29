3498c1843c384d8fa1a9e1985f54399477128361
emmashi12 <38935478+emmashi12@users.noreply.github.com>
2022-07-30 00:19:11 +0100

    prom control

--- PyTorch/SpeechSynthesis/FastPitch/inference.py
+++ PyTorch/SpeechSynthesis/FastPitch/inference.py
@@ -427,13 +427,13 @@ def main():
                     if args.cwt_prominence is True:
                         mel, mel_lens, _, pitch_pred, _, _ = generator(b['text'], **gen_kw, cwt_tgt=b['prom_upsampled'])
                         # plot pitch_pred and save the plot
-                        for i in range(args.batch_size):
-                            print(pitch_pred.shape)
-                            image = pitch_pred[i].permute(1, 2, 0)
-                            image_name = 'f0_contour_' + str(i) + '.png'
-                            plt.imshow(image)
-                            plt.show()
-                            plt.savefig(image_name)
+                        #for i in range(args.batch_size):
+                        print(pitch_pred.shape)
+                        image = pitch_pred.permute(1, 2, 0)
+                        image_name = 'f0_contour' + '.png'
+                        plt.imshow(image)
+                        plt.show()
+                        plt.savefig(image_name)
 
                         # save predicted pitch tensor
                         for j, p in enumerate(pitch_pred):
